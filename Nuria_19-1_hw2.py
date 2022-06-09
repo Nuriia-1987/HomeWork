0193803ab3b986b94274f3bb5db215b67832c6fa
Nuriia-1987 <106921164+Nuriia-1987@users.noreply.github.com>
2022-06-09 15:55:20 +0600

    Add files via upload

---
+++ Nuria_19-1_hw2.py
@@ -0,0 +1,38 @@
+
+class Company:
+    def __init__(self, company_bank, company_name):
+        self.company_bank = company_bank
+        self.company_name = company_name
+
+
+class Person(Company):
+    def __init__(self, company_bank, company_name, first_name, last_name, salary):
+        super().__init__(company_bank, company_name)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def get_salary(self):
+        if self.company_bank <= self.salary:
+            return "У компании не достаточно средств!"
+        else:
+            self.company_bank -= self.salary
+            return self.salary
+
+    def person_info(self):
+        print(f""" 
+First_Name: {self.first_name} 
+Last_name: {self.last_name}
+Person_Salary: {self.get_salary()}""")
+
+
+tom = Person(10000, "Opera", "Baron", "Tom", 2500)
+tom.person_info()
+tom.person_info()
+tom.person_info()
+tom.person_info()
+tom.person_info()
+
+
+
+
