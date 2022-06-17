d76b99f1afbb625ebc0686d58dfb3089c2abd443
Nuriia-1987 <106921164+Nuriia-1987@users.noreply.github.com>
2022-06-17 17:41:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-#HW:
-# Создать пакет - (cars)
-# создать модули такие как (create_car, get_car_info)
-# create_cars —> Создать Абстрактный класс (Car)
-# Создать методы для Car - (start_engine, stop_engine)
-# get_car_info —> def get_car_info(car): —> car_info
-# создать main.py файл и там использовать ваш пакет (cars)
 
 from cars.create_cars import Car
 
