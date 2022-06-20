efc2688ead008216f77733ad54d400a320d02c8f
Nuriia-1987 <106921164+Nuriia-1987@users.noreply.github.com>
2022-06-20 14:46:37 +0600

    Add files via upload

---
+++ Nuria_19-1_hw5.py
@@ -0,0 +1,22 @@
+
+import re
+
+number = input("введите номер машины: ")
+
+
+class ValidCarNumber:
+
+    def __init__(self):
+        self.number = number
+
+    def is_valid(self):
+        is_valid = re.search(r"(0)[1-9](KG)([0-9]{3})([A-Z]{3})", number)
+        try:
+            if is_valid.end() == len(number):
+                print("Car number is Valid!")
+            else:
+                raise ValueError
+        except:
+            print("Car number invalid!")
+
+ValidCarNumber.is_valid(number)
